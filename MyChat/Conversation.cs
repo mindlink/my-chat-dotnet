7182b6ea8ca920435290bd6de57d772dc01f5522
mindlink <mldevelopers@mindlinksoft.com>
2016-06-03 09:30:15 +0100

    Initial commit.

---
+++ MyChat/Conversation.cs
@@ -0,0 +1,35 @@
+namespace MindLink.Recruitment.MyChat
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents the model of a conversation.
+    /// </summary>
+    public sealed class Conversation
+    {
+        /// <summary>
+        /// The name of the conversation.
+        /// </summary>
+        public string name;
+
+        /// <summary>
+        /// The messages in the conversation.
+        /// </summary>
+        public IEnumerable<Message> messages;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Conversation"/> class.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the conversation.
+        /// </param>
+        /// <param name="messages">
+        /// The messages in the conversation.
+        /// </param>
+        public Conversation(string name, IEnumerable<Message> messages)
+        {
+            this.name = name;
+            this.messages = messages;
+        }
+    }
+}
---
+++ MyChat/ConversationExporterConfiguration.cs
@@ -0,0 +1,41 @@
+namespace MindLink.Recruitment.MyChat
+{
+    using System;
+
+    /// <summary>
+    /// Represents the configuration for the exporter.
+    /// </summary>
+    public sealed class ConversationExporterConfiguration
+    {
+        /// <summary>
+        /// The input file path.
+        /// </summary>
+        public string inputFilePath;
+
+        /// <summary>
+        /// The output file path.
+        /// </summary>
+        public string outputFilePath;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConversationExporterConfiguration"/> class.
+        /// </summary>
+        /// <param name="inputFilePath">
+        /// The input file path.
+        /// </param>
+        /// <param name="outputFilePath">
+        /// The output file path.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when any of the given arguments is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when any of the given arguments is empty.
+        /// </exception>
+        public ConversationExporterConfiguration(string inputFilePath, string outputFilePath)
+        {
+            this.inputFilePath = inputFilePath;
+            this.outputFilePath = outputFilePath;
+        }
+    }
+}

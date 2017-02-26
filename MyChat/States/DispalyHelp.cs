698cf6784b02d41ec7d0687be4079697a6e0ab11
n123al <nicholas@hadjimina.com>
2017-02-26 22:00:23 +0200

    Nicholas Submission
    How to use conversation command-line utility:
    
    /i : Defines the input files from where a conversation will be loaded e.g: /i inputfile.txt
    /o : Defines the output files where the current conversation will be exported in json format e.g: /o outputfile.json
    /u : Filter conversation messages that belong to the user specified  e.g: /u userid
    /w : Filter conversation messages that contained the word specified e.g: /w word
    /b : Hide specific words in the messages content (replace with *redacted*) e.g: /b word1 word2 word3
    /ct : Hide credit cart and phone like numbers in messages content e.g: /ct
    /hs : Obfuscate user ID with a unique random generated id e.g: /hs");
    /r : Adds a report to the conversation that details the most active users e.g: /r
    /? : Display help screen e.g: /?
    
    Developed by Nicholas Hadjiminas

--- MyChat/States/DispalyHelp.cs
+++ MyChat/States/DispalyHelp.cs
@@ -18,17 +18,17 @@ namespace MindLink.Recruitment.MyChat.Sates
             }
 
             // help screen
-            Console.WriteLine(" How to use conversation prosessing comand-line untility :");
+            Console.WriteLine(" How to use conversation command-line utility :");
             Console.WriteLine("----------------------------------------------------------");
             Console.WriteLine("/i       Defines the input files from where a conversation will be loaded e.g: /i inputfile.txt");
-            Console.WriteLine("/o       Defines the output files where the curent conversation will be exported in json format e.g: /o outputfile.json");
+            Console.WriteLine("/o       Defines the output files where the current conversation will be exported in json format e.g: /o outputfile.json");
             Console.WriteLine("/u       Filter conversation messages that belong to the user specified  e.g: /u userid");
             Console.WriteLine("/w       Filter conversation messages that contained the word specified e.g: /w word");
-            Console.WriteLine("/b       Hide specific words in the messages contentend (replace with *redacted*) e.g: /b word1 word2 word3");
-            Console.WriteLine("/ct      Hide credit cart and pohone like numbers in messages contentend e.g: /ct");
+            Console.WriteLine("/b       Hide specific words in the messages content (replace with *redacted*) e.g: /b word1 word2 word3");
+            Console.WriteLine("/ct      Hide credit cart and phone like numbers in messages content  e.g: /ct");
             Console.WriteLine("/hs      Obfuscate user ID with a unique random generated id e.g: /hs");
             Console.WriteLine("/r       Adds a report to the conversation that details the most active users e.g: /r");
-            Console.WriteLine("/?       Dispaly help screen e.g: /?");
+            Console.WriteLine("/?       Display help screen e.g: /?");
             Console.WriteLine("----------------------------------------------------------");
             return null;
         }

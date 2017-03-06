3e0ffdfbeea52961542915553663391046312463
robelli4711 <robelli4711@gmail.com>
2017-03-06 07:41:12 +0200

    Add files via upload

---
+++ MyChat/MyChat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyChatLibrary;
+
+namespace MyChat
+{
+    class MyChat
+    {
+        /// <summary>
+        /// The application entry point.
+        /// </summary>
+        /// <param name="args">
+        /// The command line arguments.
+        /// </param>
+        static void Main(string[] args)
+        {
+            var conversationExporter = new ConversationExporter();
+            ConversationExporterConfiguration configuration = new CommandLineArgumentParser().ParseCommandLineArguments(args);
+            conversationExporter.ExportConversation(configuration);
+        }
+
+    }
+}

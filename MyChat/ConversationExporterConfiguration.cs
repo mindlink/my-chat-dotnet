8198c0412b2c4e8a4b88d7a570d18d3c3516c7d5
Denis Podgorodnichenko <qadmium@gmail.com>
2017-04-01 17:11:43 +0300

    Fix style

--- MyChat/ConversationExporterConfiguration.cs
+++ MyChat/ConversationExporterConfiguration.cs
@@ -1,36 +1,36 @@
+using System;
+
 namespace MindLink.Recruitment.MyChat
 {
-    using System;
-
     /// <summary>
-    /// Represents the configuration for the exporter.
+    ///     Represents the configuration for the exporter.
     /// </summary>
     public sealed class ConversationExporterConfiguration
     {
         /// <summary>
-        /// The input file path.
+        ///     The input file path.
         /// </summary>
         public string inputFilePath;
 
         /// <summary>
-        /// The output file path.
+        ///     The output file path.
         /// </summary>
         public string outputFilePath;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConversationExporterConfiguration"/> class.
+        ///     Initializes a new instance of the <see cref="ConversationExporterConfiguration" /> class.
         /// </summary>
         /// <param name="inputFilePath">
-        /// The input file path.
+        ///     The input file path.
         /// </param>
         /// <param name="outputFilePath">
-        /// The output file path.
+        ///     The output file path.
         /// </param>
         /// <exception cref="ArgumentNullException">
-        /// Thrown when any of the given arguments is <c>null</c>.
+        ///     Thrown when any of the given arguments is <c>null</c>.
         /// </exception>
         /// <exception cref="ArgumentException">
-        /// Thrown when any of the given arguments is empty.
+        ///     Thrown when any of the given arguments is empty.
         /// </exception>
         public ConversationExporterConfiguration(string inputFilePath, string outputFilePath)
         {

67deb51ae10df5f69823894256a1034ca42be85f
Denis Podgorodnichenko <qadmium@gmail.com>
2017-04-02 21:58:13 +0300

    Stub for web service added

---
+++ MyChat.WebService/Controllers/ConversationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyChat.WebService.Controllers
+{
+    [RoutePrefix("conversation")]
+    public class ConversationController : ApiController
+    {
+        [HttpPost, Route("")]
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+            return this.Request.CreateResponse(HttpStatusCode.Created, value);
+        }
+    }
+}

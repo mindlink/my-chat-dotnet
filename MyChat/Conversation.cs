de0f622a99155f24723a8a4b037310d5f48dea2d
Denis Podgorodnichenko <qadmium@gmail.com>
2017-04-02 21:22:26 +0300

    Report is added to the conversation that details the most active users

--- MyChat/Conversation.cs
+++ MyChat/Conversation.cs
@@ -1,35 +1,49 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MindLink.MyChat
 {
+    public sealed class ConversationStatsItem
+    {
+        public string SenderId { get; set; }
+        public int MessagesSent { get; set; }
+    }
+
     /// <summary>
-    /// Represents the model of a conversation.
+    ///     Represents the model of a conversation.
     /// </summary>
     public sealed class Conversation
     {
         /// <summary>
-        /// The name of the conversation.
-        /// </summary>
-        public string Name { get; }
-
-        /// <summary>
-        /// The messages in the conversation.
-        /// </summary>
-        public IEnumerable<Message> Messages { get; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Conversation"/> class.
+        ///     Initializes a new instance of the <see cref="Conversation" /> class.
         /// </summary>
         /// <param name="name">
-        /// The name of the conversation.
+        ///     The name of the conversation.
         /// </param>
         /// <param name="messages">
-        /// The messages in the conversation.
+        ///     The messages in the conversation.
         /// </param>
         public Conversation(string name, IEnumerable<Message> messages)
         {
             this.Name = name;
             this.Messages = messages;
         }
+
+        /// <summary>
+        ///     The name of the conversation.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        ///     The messages in the conversation.
+        /// </summary>
+        public IEnumerable<Message> Messages { get; }
+
+        /// <summary>
+        ///     Conversation stats.
+        /// </summary>
+        public IEnumerable<ConversationStatsItem> Stats => this.Messages.GroupBy(m => m.SenderId)
+            .Select(g => new ConversationStatsItem {SenderId = g.Key, MessagesSent = g.Count()})
+            .OrderByDescending(i => i.MessagesSent);
     }
 }

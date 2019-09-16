0fe22d1873f03f1c992fae8966c83588ef3635c0
Jake Baum <jake@jakebaum.com>
2019-09-16 17:28:35 +0100

    Added test for blacklist number function.

--- MyChat.Tests/ConversationTests.cs
+++ MyChat.Tests/ConversationTests.cs
@@ -77,12 +77,12 @@ namespace MindLink.Recruitment.MyChat.Tests
         public void TestBlacklistWord()
         {
             Conversation conversation = readConversation();
-            conversation.BlacklistWord("pie");
+            Conversation blacklisted = conversation.BlacklistWord("pie");
 
             //Test conversation name is correct
-            Assert.AreEqual("My Conversation", conversation.name);
+            Assert.AreEqual("My Conversation", blacklisted.name);
 
-            List<Message> messages = (List<Message>)conversation.messages;
+            List<Message> messages = (List<Message>)blacklisted.messages;
 
             //Test there are correct amount of messages
             Assert.AreEqual(7, messages.Count);
@@ -115,6 +115,45 @@ namespace MindLink.Recruitment.MyChat.Tests
             Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470915), messages[6].timestamp);
             Assert.AreEqual("angus", messages[6].senderId);
             Assert.AreEqual("YES! I'm the head *redacted* eater there...", messages[6].content);
+        }
+
+        [TestMethod]
+        public void TestBlacklistNumber()
+        {
+            Message message1 = new Message(DateTimeOffset.FromUnixTimeSeconds(1448470901), "bob", "My phone number is +447248833902");
+            Message message2 = new Message(DateTimeOffset.FromUnixTimeSeconds(1448470907), "dave", "My credit card number is 2938475858584939.");
+            Message message3 = new Message(DateTimeOffset.FromUnixTimeSeconds(1448470912), "roger", "This is not my phone number: 07336621783, is your credit card number really 2938475858584939?");
+
+            List<Message> messages = new List<Message>();
+            messages.Add(message1);
+            messages.Add(message2);
+            messages.Add(message3);
+
+            Conversation conversation = new Conversation("Number discussion", messages);
+
+            Conversation newConversation = conversation.BlacklistPhoneAndCC();
+
+            //Test conversation name is correct
+            Assert.AreEqual("Number discussion", newConversation.name);
+
+            messages = (List<Message>)newConversation.messages;
+
+            //Test there is correct amount of messages
+            Assert.AreEqual(3, messages.Count);
+
+            //Test messages are correct
+            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470901), messages[0].timestamp);
+            Assert.AreEqual("bob", messages[0].senderId);
+            Assert.AreEqual("My phone number is *redacted*", messages[0].content);
+
+            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470907), messages[1].timestamp);
+            Assert.AreEqual("dave", messages[1].senderId);
+            Assert.AreEqual("My credit card number is *redacted*.", messages[01].content);
+
+            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470912), messages[2].timestamp);
+            Assert.AreEqual("roger", messages[2].senderId);
+            Assert.AreEqual("This is not my phone number: *redacted*, is your credit card number really *redacted*?", messages[2].content);
+
         }
     }
 }

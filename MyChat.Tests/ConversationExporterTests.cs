39e382da08d18aba3bd121658a1a8eb027c3b494
Luke Terry <Luke.Terry@mindlinksoft.com>
2020-07-15 10:08:03 +0100

    Move projects over to dotnet core
    Modernise by switching to dotnet core and nunit.

--- MyChat.Tests/ConversationExporterTests.cs
+++ MyChat.Tests/ConversationExporterTests.cs
@@ -2,63 +2,63 @@
 using System.Linq;
 using MyChat;
 using Newtonsoft.Json;
+using NUnit.Framework;
 
 namespace MindLink.Recruitment.MyChat.Tests
 {
     using System;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     /// <summary>
     /// Tests for the <see cref="ConversationExporter"/>.
     /// </summary>
-    [TestClass]
+    [TestFixture]
     public class ConversationExporterTests
     {
         /// <summary>
         /// Tests that exporting the conversation exports conversation.
         /// </summary>
-        [TestMethod]
+        [Test]
         public void ExportingConversationExportsConversation()
         {
-            ConversationExporter exporter = new ConversationExporter();
+            var exporter = new ConversationExporter();
 
             exporter.ExportConversation("chat.txt", "chat.json");
 
             var serializedConversation = new StreamReader(new FileStream("chat.json", FileMode.Open)).ReadToEnd();
 
-            Conversation savedConversation = JsonConvert.DeserializeObject<Conversation>(serializedConversation);
+            var savedConversation = JsonConvert.DeserializeObject<Conversation>(serializedConversation);
 
-            Assert.AreEqual("My Conversation", savedConversation.name);
+            Assert.That(savedConversation.name, Is.EqualTo("My Conversation"));
 
             var messages = savedConversation.messages.ToList();
 
-            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470901), messages[0].timestamp);
-            Assert.AreEqual("bob", messages[0].senderId);
-            Assert.AreEqual("Hello there!", messages[0].content);
+            Assert.That(messages[0].timestamp, Is.EqualTo(DateTimeOffset.FromUnixTimeSeconds(1448470901)));
+            Assert.That(messages[0].senderId, Is.EqualTo("bob"));
+            Assert.That(messages[0].content, Is.EqualTo("Hello there!"));
 
-            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470905), messages[1].timestamp);
-            Assert.AreEqual("mike", messages[1].senderId);
-            Assert.AreEqual("how are you?", messages[1].content);
+            Assert.That(messages[1].timestamp, Is.EqualTo(DateTimeOffset.FromUnixTimeSeconds(1448470905)));
+            Assert.That(messages[1].senderId, Is.EqualTo("mike"));
+            Assert.That(messages[1].content, Is.EqualTo("how are you?"));
 
-            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470906), messages[2].timestamp);
-            Assert.AreEqual("bob", messages[2].senderId);
-            Assert.AreEqual("I'm good thanks, do you like pie?", messages[2].content);
+            Assert.That(messages[2].timestamp, Is.EqualTo(DateTimeOffset.FromUnixTimeSeconds(1448470906)));
+            Assert.That(messages[2].senderId, Is.EqualTo("bob"));
+            Assert.That(messages[2].content, Is.EqualTo("I'm good thanks, do you like pie?"));
 
-            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470910), messages[3].timestamp);
-            Assert.AreEqual("mike", messages[3].senderId);
-            Assert.AreEqual("no, let me ask Angus...", messages[3].content);
+            Assert.That(messages[3].timestamp, Is.EqualTo(DateTimeOffset.FromUnixTimeSeconds(1448470910)));
+            Assert.That(messages[3].senderId, Is.EqualTo("mike"));
+            Assert.That(messages[3].content, Is.EqualTo("no, let me ask Angus..."));
 
-            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470912), messages[4].timestamp);
-            Assert.AreEqual("angus", messages[4].senderId);
-            Assert.AreEqual("Hell yes! Are we buying some pie?", messages[4].content);
+            Assert.That(messages[4].timestamp, Is.EqualTo(DateTimeOffset.FromUnixTimeSeconds(1448470912)));
+            Assert.That(messages[4].senderId, Is.EqualTo("angus"));
+            Assert.That(messages[4].content, Is.EqualTo("Hell yes! Are we buying some pie?"));
 
-            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470914), messages[5].timestamp);
-            Assert.AreEqual("bob", messages[5].senderId);
-            Assert.AreEqual("No, just want to know if there's anybody else in the pie society...", messages[5].content);
+            Assert.That(messages[5].timestamp, Is.EqualTo(DateTimeOffset.FromUnixTimeSeconds(1448470914)));
+            Assert.That(messages[5].senderId, Is.EqualTo("bob"));
+            Assert.That(messages[5].content, Is.EqualTo("No, just want to know if there's anybody else in the pie society..."));
 
-            Assert.AreEqual(DateTimeOffset.FromUnixTimeSeconds(1448470915), messages[6].timestamp);
-            Assert.AreEqual("angus", messages[6].senderId);
-            Assert.AreEqual("YES! I'm the head pie eater there...", messages[6].content);
+            Assert.That(messages[6].timestamp, Is.EqualTo(DateTimeOffset.FromUnixTimeSeconds(1448470915)));
+            Assert.That(messages[6].senderId, Is.EqualTo("angus"));
+            Assert.That(messages[6].content, Is.EqualTo("YES! I'm the head pie eater there..."));
         }
     }
 }

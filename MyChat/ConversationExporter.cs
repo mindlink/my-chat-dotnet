5132a82cdcd3646330282940eda2f6496d0ae9df
David Godwin <dgodwin26@yahoo.co.uk>
2020-09-14 16:49:25 +0100

    Pull out ReadConversation from ExportConversation Seems to me Reading and Exporting are different things and I'd rather the functions, as much as possible, do one significant thing and should be written in that way.

--- MyChat/ConversationExporter.cs
+++ MyChat/ConversationExporter.cs
@@ -8,64 +8,25 @@
     using MindLink.Recruitment.MyChat;
     using Newtonsoft.Json;
 
-    /// <summary>
-    /// Represents a conversation exporter that can read a conversation and write it out in JSON.
-    /// </summary>
     public sealed class ConversationExporter
     {
-        /// <summary>
-        /// The application entry point.
-        /// </summary>
-        /// <param name="args">
-        /// The command line arguments.
-        /// </param>
         static void Main(string[] args)
         {
             var conversationExporter = new ConversationExporter();
             ConversationExporterConfiguration configuration = new CommandLineArgumentParser().ParseCommandLineArguments(args);
 
-            conversationExporter.ExportConversation(configuration.inputFilePath, configuration.outputFilePath);
+            Conversation conversation = conversationExporter.ReadConversation(configuration.inputFilePath);
+            
+            conversationExporter.ExportConversation(conversation, configuration.inputFilePath, configuration.outputFilePath);
         }
-
-        /// <summary>
-        /// Exports the conversation at <paramref name="inputFilePath"/> as JSON to <paramref name="outputFilePath"/>.
-        /// </summary>
-        /// <param name="inputFilePath">
-        /// The input file path.
-        /// </param>
-        /// <param name="outputFilePath">
-        /// The output file path.
-        /// </param>
-        /// <exception cref="ArgumentException">
-        /// Thrown when a path is invalid.
-        /// </exception>
-        /// <exception cref="Exception">
-        /// Thrown when something bad happens.
-        /// </exception>
-        public void ExportConversation(string inputFilePath, string outputFilePath)
+        
+        public void ExportConversation(Conversation c, string inputFilePath, string outputFilePath)
         {
-            Conversation conversation = this.ReadConversation(inputFilePath);
-
-            this.WriteConversation(conversation, outputFilePath);
-
+            //Takes in a conversation and writes it to an output file path.
+            WriteConversation(c, outputFilePath);
             Console.WriteLine("Conversation exported from '{0}' to '{1}'", inputFilePath, outputFilePath);
         }
-
-        /// <summary>
-        /// Helper method to read the conversation from <paramref name="inputFilePath"/>.
-        /// </summary>
-        /// <param name="inputFilePath">
-        /// The input file path.
-        /// </param>
-        /// <returns>
-        /// A <see cref="Conversation"/> model representing the conversation.
-        /// </returns>
-        /// <exception cref="ArgumentException">
-        /// Thrown when the input file could not be found.
-        /// </exception>
-        /// <exception cref="Exception">
-        /// Thrown when something else went wrong.
-        /// </exception>
+        
         public Conversation ReadConversation(string inputFilePath)
         {
             try
@@ -95,22 +56,7 @@
                 throw new Exception("Something went wrong in the IO.");
             }
         }
-
-        /// <summary>
-        /// Helper method to write the <paramref name="conversation"/> as JSON to <paramref name="outputFilePath"/>.
-        /// </summary>
-        /// <param name="conversation">
-        /// The conversation.
-        /// </param>
-        /// <param name="outputFilePath">
-        /// The output file path.
-        /// </param>
-        /// <exception cref="ArgumentException">
-        /// Thrown when there is a problem with the <paramref name="outputFilePath"/>.
-        /// </exception>
-        /// <exception cref="Exception">
-        /// Thrown when something else bad happens.
-        /// </exception>
+        
         public void WriteConversation(Conversation conversation, string outputFilePath)
         {
             try

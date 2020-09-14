5056e59098068005ddb19f881f2a95cc66988507
David Godwin <dgodwin26@yahoo.co.uk>
2020-09-14 15:27:10 +0100

    Private instance variables + comment removal. Don't think, at this stage anyway, we need to have all of this accessible outside of the class. We can clean up setting/getting with C# properties, so let's do that. Capitalised instance variables and kept constructor args lower to make it easier to distinguish between them. Got rid of a load of comments to make this easier to read. Reminded of Pike and Kernighan's 'Practice of Programming': use comments when you really need them!

--- MyChat/Conversation.cs
+++ MyChat/Conversation.cs
@@ -1,35 +1,16 @@
 namespace MindLink.Recruitment.MyChat
 {
     using System.Collections.Generic;
-
-    /// <summary>
-    /// Represents the model of a conversation.
-    /// </summary>
+    
     public sealed class Conversation
     {
-        /// <summary>
-        /// The name of the conversation.
-        /// </summary>
-        public string name;
+        private string Name { get; set; }
+        private IEnumerable<Message> Messages;
 
-        /// <summary>
-        /// The messages in the conversation.
-        /// </summary>
-        public IEnumerable<Message> messages;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Conversation"/> class.
-        /// </summary>
-        /// <param name="name">
-        /// The name of the conversation.
-        /// </param>
-        /// <param name="messages">
-        /// The messages in the conversation.
-        /// </param>
         public Conversation(string name, IEnumerable<Message> messages)
         {
-            this.name = name;
-            this.messages = messages;
+            Name = name;
+            Messages = messages;
         }
     }
 }

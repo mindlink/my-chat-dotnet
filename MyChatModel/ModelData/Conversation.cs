7530db22529ceea1d0d950685c7b6bca9445b854
Yrrah98 <harryjones101998@hotmail.com>
2020-09-18 18:32:32 +0100

    Wouldn't add conversation.cs changes to the staged commits, so commiting this after, no changes made since last commit

--- MyChatModel/ModelData/Conversation.cs
+++ MyChatModel/ModelData/Conversation.cs
@@ -9,7 +9,11 @@
         /// The name of the conversation.
         /// </summary>
         public string Name { get; }
-
+        /// <summary>
+        /// A list of strings for the filters to write any messages into
+        /// in case of certain scenarios
+        /// </summary>
+        public IList<string> FilterMessage { get; set; }
         /// <summary>
         /// The messages in the conversation.
         /// </summary>
@@ -27,7 +31,15 @@
         {
             this.Name = name;
             this.Messages = messages;
+        }
 
+        public void AddFilterMessage(string convoMsg) 
+        {
+            // IF FilterMessage is null, then initialise it
+            if (FilterMessage == null)
+                FilterMessage = new List<string>();
+            // ADD the message to the conversations message
+            FilterMessage.Add(convoMsg);
         }
     }
 }

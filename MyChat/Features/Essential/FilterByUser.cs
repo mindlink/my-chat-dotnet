5c0f1e40a2c28061fb0c24238a3acf937865b3bc
Yrrah98 <harryjones101998@hotmail.com>
2020-09-18 18:30:51 +0100

    Added a new variable into the Conversation class, called FilterMessage. Filters will apply a message in certain scenarios, e.g. FilterByUser class finds no one by the name specified then it out puts No user by the name <name> was found

--- MyChat/Features/Essential/FilterByUser.cs
+++ MyChat/Features/Essential/FilterByUser.cs
@@ -29,23 +29,45 @@
 
         public Conversation ApplyFilter(Conversation conversation) 
         {
+            // INTIIALISE a Conversation called filteredConversation
+            Conversation filteredConversation;
+            // INSTANTIATE a bool to say whether the user name was valid or not
+            bool validUser = false;
             // INITIALISE a local list of type Message called filteredMessages
             IList<Message> filteredMessages = new List<Message>();
-
             // FOREACH through the messages in the conversation
             foreach (Message msg in conversation.Messages) 
             {
                 // IF the message sender ID matches the id of the user
                 if (msg.SenderId == user) 
                 {
+                    validUser = true;
                     // ADD that message to the list of messages
                     filteredMessages.Add(msg);
                 }
             }
 
+            filteredConversation = new Conversation(conversation.Name, filteredMessages);
+
+            // IF the username is not valid
+            if (!validUser)
+            {
+                if (user == "" || user == " ")
+                {
+                    throw new ArgumentNullException("No username was supplied for the filter to use");
+                }
+                else 
+                {
+                    string conversationMessage = "No user by the name " + user + " was found";
+
+                    filteredConversation.AddFilterMessage(conversationMessage);
+                }
+                
+            }
+
             // RETURN a new conversation, passing the conversation name to the constructor and the filtered
             // list of messages
-            return new Conversation(conversation.Name, filteredMessages);
+            return filteredConversation;
         }
     }
 }

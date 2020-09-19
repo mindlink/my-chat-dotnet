9320abc71982cdb78cf51bd8a57f465ddb4c165b
Yrrah98 <harryjones101998@hotmail.com>
2020-09-19 16:36:04 +0100

    Same as previous commit, stuff wasn't staged

---
+++ MyChat/CustomExceptions/UnacceptableFilterArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MindLink.Recruitment.MyChat.CustomExceptions
+{
+    /// <summary>
+    /// Custom exception, if the user tries to supply/forgets to supply an argument
+    /// and instead supplies a filter as an argument
+    /// </summary>
+    [Serializable]
+    public class UnacceptableFilterArgs : Exception
+    {
+
+        public UnacceptableFilterArgs() : base() 
+        {
+
+        }
+
+        public UnacceptableFilterArgs(string message) : base(message) 
+        {
+
+        }
+
+        public UnacceptableFilterArgs(string message, Exception innerException) : base(message, innerException) 
+        {
+
+        }
+    }
+}

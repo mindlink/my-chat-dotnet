5be9222c2dd6d23e9d38b9be7ae046bd818e383a
David Godwin <dgodwin26@yahoo.co.uk>
2020-09-20 22:05:20 +0100

    Stop attempting to access arg in the Exception message

--- MyChat/CommandLineArgumentParser.cs
+++ MyChat/CommandLineArgumentParser.cs
@@ -9,8 +9,9 @@ namespace MindLink.Recruitment.MyChat
         public string ParseCommandLineArguments(string[] arguments)
         {
             if(arguments.Length != 1){
-                throw new ArgumentException($"The {arguments[0]} arguments when this tool needs 1 argument, a json config file");
+                throw new ArgumentException($"You have provided the wrong number of arguments. This tool needs 1 argument, a json config file");
             }
+
             if (Path.GetExtension(arguments[0]) != ".json")
             {
                 throw new ArgumentException("You need to provide a JSON configuration file for this tool to work.");

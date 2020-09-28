a5eaee955b4d2911f2135b8427532eca99ee2dec
“David <dgodwin26@yahoo.co.uk>
2020-09-28 17:08:29 +0100

    Push unsuccessful tests to 'show your working' Admittedly didn't get these to work as I wanted. Idea was to move up a level and start testing the application end to end rather than constantly having to run the code myself because that can get tedious and likely will resort in human error. Something to discuss(!).

--- MyChat.Tests/ConversationExporterTests.cs
+++ MyChat.Tests/ConversationExporterTests.cs
@@ -1,66 +1,64 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using Microsoft.VisualStudio.TestPlatform.Utilities;
-using MyChat;
-using Newtonsoft.Json;
-using NUnit.Framework;
-
-
-namespace MindLink.Recruitment.MyChat.Tests
-{
-     using System;
-
-     [TestFixture]
-     public class ConversationExporterTests
-     {
-         [Test]
-         public void TitleOfExportedConversationIsCorrect()
-         {
-             var exporter = new ConversationExporter();
-
-             var reader = TextReaderFactory.GetStreamReader("chat.txt", FileMode.Open,
-                 FileAccess.Read, Encoding.ASCII);
-
-             var writer = TextWriterFactory.GetStreamWriter("output.json", FileMode.Create, FileAccess.ReadWrite);
-
-             exporter.WriteConversation(writer, exporter.ExtractConversation(reader, null, null));
-
-             var serializedConversation = new StreamReader(new FileStream("output.json", FileMode.Open)).ReadToEnd();
-             Console.WriteLine(serializedConversation);
-             var savedConversation = JsonConvert.DeserializeObject<Conversation>(serializedConversation);
-
-             Assert.That(savedConversation.Name, Is.EqualTo("My Conversation"));
-         }
-
-         [Test]
-         public void first_message_item_is_from_david()
-         {
-             var cp = new ConversationExporter();
-             IFilterer dg = new UserNameFilterer("david");
-             
-             TextReader reader = TextReaderFactory.GetStreamReader("chat.txt", FileMode.Open,
-                 FileAccess.Read, Encoding.ASCII);
-
-             TextWriter writer = TextWriterFactory.GetStreamWriter("output.json", FileMode.Create, FileAccess.ReadWrite);
-
-             Conversation conversation = cp.ExtractConversation(reader, dg, null);
-             
-             writer = TextWriterFactory.GetStreamWriter("output.json", FileMode.Create,
-                 FileAccess.ReadWrite);
-             
-             cp.WriteConversation(writer, conversation);
-             
-             var serializedConversation = new StreamReader(new FileStream("output.json", FileMode.Open)).ReadToEnd();
-             
-             var convo = JsonConvert.DeserializeObject<Conversation>(serializedConversation);
-             
-             string cname = convo.Name;
-             Console.WriteLine(cname);
-
-             // Console.WriteLine(convo.Messages.First().senderId);
-             
-         } 
-     }
-}
+﻿// using System.Collections.Generic;
+// using System.IO;
+// using System.Linq;
+// using System.Text;
+// using Microsoft.VisualStudio.TestPlatform.Utilities;
+// using MyChat;
+// using Newtonsoft.Json;
+// using NUnit.Framework;
+//
+//
+// namespace MindLink.Recruitment.MyChat.Tests
+// {
+//      using System;
+//
+//      [TestFixture]
+//      public class ConversationExporterTests
+//      {
+//          [Test]
+//          public void TitleOfExportedConversationIsCorrect()
+//          {
+//              var exporter = new ConversationExporter();
+//
+//              var reader = TextReaderFactory.GetStreamReader("chat.txt", FileMode.Open,
+//                  FileAccess.Read, Encoding.ASCII);
+//
+//              var writer = TextWriterFactory.GetStreamWriter("output.json", FileMode.Create, FileAccess.ReadWrite);
+//
+//              exporter.WriteConversation(writer, exporter.ExtractConversation(reader, null, null));
+//
+//              var serializedConversation = new StreamReader(new FileStream("output.json", FileMode.Open)).ReadToEnd();
+//              Console.WriteLine(serializedConversation);
+//              var savedConversation = JsonConvert.DeserializeObject<Conversation>(serializedConversation);
+//
+//              Assert.That(savedConversation.Name, Is.EqualTo("My Conversation"));
+//          }
+//
+//          [Test]
+//          public void first_message_item_is_from_david()
+//          {
+//              var cp = new ConversationExporter();
+//              IFilterer dg = new UserNameFilterer("david");
+//              
+//              TextReader reader = TextReaderFactory.GetStreamReader("chat.txt", FileMode.Open,
+//                  FileAccess.Read, Encoding.ASCII);
+//
+//              TextWriter writer = TextWriterFactory.GetStreamWriter("output.json", FileMode.Create, FileAccess.ReadWrite);
+//
+//              Conversation conversation = cp.ExtractConversation(reader, dg, null);
+//              
+//              writer = TextWriterFactory.GetStreamWriter("output.json", FileMode.Create,
+//                  FileAccess.ReadWrite);
+//              
+//              cp.WriteConversation(writer, conversation);
+//              
+//              var serializedConversation = new StreamReader(new FileStream("output.json", FileMode.Open)).ReadToEnd();
+//              
+//              var convo = JsonConvert.DeserializeObject<Conversation>(serializedConversation);
+//              
+//              string cname = convo.Name;
+//              Console.WriteLine(cname);
+//
+//          } 
+//      }
+// }
